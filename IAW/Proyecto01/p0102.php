f49940e5e8b504a03a870ae5f62a91860ca58525
poliestireno <poliestireno@gmail.com>
2026-09-25 13:56:43 +0200

    matriz 3x3 aletoria

---
+++ IAW/Proyecto01/p0102.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+ MATRIZ 3x3 de numeros aletorios en 1 y 100. 
+
+    <h1>ALBERTO F. es un tío de <?php echo rand(0,10) ?></h1>
+</body>
+</html>
